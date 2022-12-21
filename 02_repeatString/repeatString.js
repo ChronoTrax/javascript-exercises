241ebe9441c5fef1d4d4fcd11d07be08c935345d
Durval <55108934+ChronoTrax@users.noreply.github.com>
2022-12-21 17:47:12 -0500

    Completed 02_repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if (!Number.isInteger(count) || count < 0) {
+        return "ERROR"
+    }
 
+    let newStr = ""
+    for (let i = 0; i < count; i++) {
+        newStr += str
+    }
+    return newStr
 };
 
 // Do not edit below this line
