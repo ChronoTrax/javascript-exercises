5704ba189ef9e42c2d1f21b0b9cd1539d1987ed7
Durval <55108934+ChronoTrax@users.noreply.github.com>
2022-12-21 19:14:46 -0500

    Completed 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        let valid = true
+        for (let j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) {
+                valid = false
+                break
+            }
+        }
+        if (valid) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
 };
 
 // Do not edit below this line
