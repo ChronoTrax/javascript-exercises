7acbea649eddace24a8ee1e3cfae08d93b391a0d
Durval <55108934+ChronoTrax@users.noreply.github.com>
2022-12-21 19:22:31 -0500

    Completed 05_sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || num1 < 0 || !Number.isInteger(num2) || num2 < 0) {
+        return "ERROR"
+    }
 
+    let sum = 0
+
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
