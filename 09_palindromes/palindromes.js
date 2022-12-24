36991bd4d5eddb2daee23a7536e94c3a59f8b5d9
Durval <55108934+ChronoTrax@users.noreply.github.com>
2022-12-23 21:22:48 -0500

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let original = [];
+    let size = 0;
+    for (let i = 0; i < str.length; i++) {
+        let character = str.toLowerCase().charCodeAt(i);
+        if (!(character > 96 && character < 123)) { // lower alpha (a-z)
+            continue;
+        }
+        original[size++] = character;
+    }
+    size = 0;
+    for (let i = str.length - 1; i >= 0; i--) {
+        let character = str.toLowerCase().charCodeAt(i);
+        if (!(character > 96 && character < 123)) { // lower alpha (a-z)
+            continue;
+        }
+        if (original[size++] !== character) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
