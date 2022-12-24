82964d633f329710ecc1bcf42315d7a6a52b7fe9
Durval <55108934+ChronoTrax@users.noreply.github.com>
2022-12-23 21:47:16 -0500

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+            let ageA = a.hasOwnProperty('yearOfDeath') ? a.yearOfDeath - a.yearOfBirth : new Date().getFullYear() - a.yearOfBirth;
+            let ageB = b.hasOwnProperty('yearOfDeath') ? b.yearOfDeath - b.yearOfBirth : new Date().getFullYear() - b.yearOfBirth;
+            return ageA < ageB ? 1 : -1
+        })[0];
 };
 
 // Do not edit below this line
